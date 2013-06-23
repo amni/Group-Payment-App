debeed20c6e7abdf3bebe085d825a67295550aaa
Matthew Ebeweber <mebeweber@gmail.com>
2013-06-23 15:06:14 -0700

    added basic templates and views, passing information down

--- grouppay/Groups/urls.py
+++ grouppay/Groups/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 from groups import views
 
 urlpatterns = patterns('',
-   # /me
+   # /groups/
    url(r'^$', views.index, name='index'),
+
+   # /groups/group_id
+   url(r'^(?P<group_id>\d+)/$', views.detail, name='detail'),
 )
--- grouppay/Groups/views.py
+++ grouppay/Groups/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import Http404
 
-from groups.models import Member
+from groups.models import Member, Group, Transaction
 
-# Index will take you to an index of your groups
+import pdb;
 def index(request):
-   member = Member.objects.get(id=request.user.id)
-   context = {'member' : member}
+   try:
+      member = Member.objects.get(id=request.user.id)
+      groups = member.groups.all()
+      context = {'member' : member, 'groups': groups}
+   except Member.DoesNotExist:
+      context = {}
    return render(request, 'groups/index.html', context)
+
+def detail(request, group_id):
+   try:
+      group = Group.objects.get(id=group_id)
+      member = Member.objects.get(id=request.user.id)
+      context = {'member': member, 'group': group}
+   except Group.DoesNotExist:
+      raise Http404
+   return render(request, 'groups/detail.html', context)
